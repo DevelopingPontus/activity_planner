03fdc28fce7cee51c87bf39ae2126fa009448622
DevelopingPontus <ipont35@gmail.com>
2026-05-29 12:32:07 +0200

    init

---
+++ src/test/java/chasky/activity_planner/ActivityPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package chasky.activity_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
